7473acdc7d90bb50debb189cd2e4ebd3f80f690a
Reza Bagheri <bagherbagherreza@gmail.com>
2022-08-29 18:51:35 +0430

    Initial Repo

---
+++ RemoveDuplicatesFromSortedList/RemoveDuplicatesFromSortedList/LinkedList.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RemoveDuplicatesFromSortedList
+{
+    public class Solution
+    {
+        public ListNode DeleteDuplicates(ListNode head)
+        {
+            if (head == null)
+            {
+                return head;
+            }
+
+            ListNode node = head;
+
+            while (node.next != null)
+            {
+                if (node.val == node.next.val)
+                {
+                    node.next = node.next.next;
+                }
+                else
+                {
+                    node = node.next;
+                }
+            }
+
+            return head;
+        }
+    }
+}
---
+++ RemoveDuplicatesFromSortedList/RemoveDuplicatesFromSortedList/Program.cs
@@ -0,0 +1,19 @@
+﻿using RemoveDuplicatesFromSortedList;
+
+//ListNode f = new ListNode() { val = 3, next = null };
+//ListNode e = new ListNode() { val = 3, next = f };
+//ListNode d = new ListNode() { val = 3, next = e };
+//ListNode c = new ListNode() { val = 2, next = d};
+ListNode b = new ListNode() { val = 2, next = null };
+ListNode a = new ListNode() { val = 1, next = b };
+ListNode head = new ListNode() { val = 1, next = a};
+
+Solution solution = new Solution();
+solution.DeleteDuplicates(head);
+//while(index > 0)
+//{
+//    ListNode item = new ListNode();
+//    item.val = 1;
+//    head.next = item;
+//    head = item;
+//}
